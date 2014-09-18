42c4bea3e87687291d4525185221ba31031b7df7
Vera Tkachenko <ivira@macpaw.com>
2014-09-18 21:14:47 +0300

    Adapt height of row to the height of text.

--- Declarations/Declarations/DCCategoryViewController.m
+++ Declarations/Declarations/DCCategoryViewController.m
@@ -31,6 +31,34 @@
 
 #pragma mark - TableView datasource/delegate
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    DCValue *value = (indexPath.section == 0 ? self.category.values : self.category.familyValues)[indexPath.row];
+    if (!value.title || [value isKindOfClass:[DCVehicle class]])
+    {
+        return 60;
+    }
+    
+    static NSString *CellIdentifier = @"ValueIdentifier";
+    DCValueCellView *cell = (DCValueCellView *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    CGSize constrainedSize = CGSizeMake(cell.titleLabel.frame.size.width, 9999);
+    
+    NSDictionary *attributesDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                          [UIFont fontWithName:@"HelveticaNeue-Bold" size:13.0], NSFontAttributeName,
+                                          nil];
+    
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:value.title attributes:attributesDictionary];
+    
+    CGRect requiredHeight = [string boundingRectWithSize:constrainedSize options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    
+    if (requiredHeight.size.width > cell.titleLabel.frame.size.width) {
+        requiredHeight = CGRectMake(0, 0, cell.titleLabel.frame.size.width, requiredHeight.size.height);
+    }
+    
+    return 60.0 + requiredHeight.size.height;
+}
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 2;
