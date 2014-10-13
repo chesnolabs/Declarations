57209ffecf2431353887d496e55f76de687c3979
Vera Tkachenko <ivira@macpaw.com>
2014-10-14 00:24:24 +0300

    Basic About screen.

---
+++ Declarations/Declarations/DCAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  DCAboutViewController.h
+//  Declarations
+//
+//  Created by Vera Tkachenko on 13.10.14.
+//  Copyright (c) 2014 Chesno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCAboutViewController : UIViewController
+
+@end
